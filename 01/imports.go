0fd5a2ff5b6acdbe22fbf889c6fcf1a16dbc9843
chichoon <chichoon.choi@gmail.com>
2021-09-18 23:52:20 +0900

    [Go] ADD: 패키지와 변수 함수 (wip)

---
+++ 01/imports.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("제곱근 2: %g\n", math.Sqrt(2))
+}
---
+++ 01/packages.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("치춘이가 좋아하는 랜덤게임 : ", rand.Intn(10))
+}
